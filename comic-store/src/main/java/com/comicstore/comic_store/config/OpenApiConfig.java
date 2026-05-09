323cc33dd0f5226fa6928025f26dad4a6f866fa3
Navil Hernandez <navilhrnndz@gmail.com>
2026-05-09 19:09:01 -0400

    Fix Swagger server URL to use CloudFront origin

--- comic-store/src/main/java/com/comicstore/comic_store/config/OpenApiConfig.java
+++ comic-store/src/main/java/com/comicstore/comic_store/config/OpenApiConfig.java
@@ -5,15 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(allowedOrigin))
                 .info(new Info()
                         .title("Comic Store API")
                         .description("Inventory management REST API for a comic book store. " +
